3b57f96acc9b8e469de66be3ca327723215937e9
rohitverma007 <rohitverma@live.ca>
2025-04-11 00:41:06 +1200

    adding shadcn card + updating existing Sheet component to use the shadcn card component

--- apps/next/components/Sheet.tsx
+++ apps/next/components/Sheet.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Sheet({
   primary = false,
@@ -10,11 +11,13 @@ export default function Sheet({
   actions?: React.ReactNode;
 }) {
   return (
-    <div className={`px-3 py-6 md:px-16 ${primary ? "bg-blue-50" : "bg-gray-100"}`}>
-      <div className="flex max-w-(--breakpoint-xl) flex-col items-center gap-4 sm:flex-row sm:justify-between">
-        <div className="grow">{children}</div>
-        {actions ? <div className="flex flex-row flex-wrap gap-3 sm:flex-row-reverse">{actions}</div> : null}
-      </div>
-    </div>
+    <Card className={`px-3 py-6 md:px-16 border-0 rounded-none shadow-none ${primary ? "bg-blue-50" : "bg-gray-100"}`}>
+      <CardContent className="p-0">
+        <div className="flex max-w-(--breakpoint-xl) flex-col items-center gap-4 sm:flex-row sm:justify-between">
+          <div className="grow">{children}</div>
+          {actions ? <div className="flex flex-row flex-wrap gap-3 sm:flex-row-reverse">{actions}</div> : null}
+        </div>
+      </CardContent>
+    </Card>
   );
 }
